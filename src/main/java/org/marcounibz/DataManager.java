a43266cf07e50640191ae0b28933f3c7ab28d38a
rialii <alice.rizzo@student.unibz.it>
2023-11-24 15:49:16 +0100

    chore: added class dataManager that will manage data and merge them

---
+++ src/main/java/org/marcounibz/DataManager.java
@@ -0,0 +1,37 @@
+package org.marcounibz;
+
+
+import org.marcounibz.configurationHelper.ConfiguratorReader;
+import org.marcounibz.configurationHelper.OpenDataHubApiConfig;
+
+import java.util.List;
+import java.util.Map;
+
+//this class will handle the datas and return
+public class DataManager {
+    Mobility mobility;
+    Tourism tourism;
+    OpenDataHubApiConfig mobilityConfig;
+    OpenDataHubApiConfig tourismConfig;
+    public DataManager(Mobility mobility, Tourism tourism){ //will they both be of type OpenDataHubClient???
+
+        this.mobility = mobility;
+        this.tourism = tourism;
+    }
+
+    //not sure about the position of this and the interaction between classes!
+    public void setConfiguration(){
+        ConfiguratorReader configuratorReader = new ConfiguratorReader();
+        this.mobilityConfig = configuratorReader.getMobilityConfig();
+        this.tourismConfig = configuratorReader.getTourismConfigConfig();
+    }
+    //get data from api and merge
+    public List<Map<String, Object>> mergeData() throws Exception {
+        List<Map<String, Object>> returnValue = null;
+        List<Map<String, Object>> data1 = mobility.fetchDataFromApi(mobilityConfig);
+        List<Map<String, Object>> data2 = tourism.fetchDataFromApi(tourismConfig);
+        //merge somehow data
+
+        return returnValue;
+    }
+}
